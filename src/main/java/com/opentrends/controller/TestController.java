04566c5a7b87e5fb3a8fba0373e96fd0f914fcb5
akhilsajeev <akhil.m.sajeev@gmail.com>
2018-03-16 11:31:41 +0530

    Test Commit

--- src/main/java/com/opentrends/controller/TestController.java
+++ src/main/java/com/opentrends/controller/TestController.java
@@ -6,11 +6,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 	
-	@RequestMapping("/")
-	public String welcomePage(){
-		return "Welcome";
-	}
-	
 	@RequestMapping("/load")
 	public String getLoad(){
 		return "Loading Load Page.....";
