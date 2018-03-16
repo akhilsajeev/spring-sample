1fb40d0c832a8ff2bf1da1c2f3573ef7d141ea57
akhilsajeev <akhil.m.sajeev@gmail.com>
2018-03-16 10:44:09 +0530

    error fixed

--- src/main/java/com/opentrends/controller/TestController.java
+++ src/main/java/com/opentrends/controller/TestController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
 	
 	@RequestMapping("/")
-	public String getLoad(){
+	public String welcomePage(){
 		return "Welcome";
 	}
 	
